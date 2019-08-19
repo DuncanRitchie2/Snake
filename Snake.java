999952851e147d39f8c5aa5986adbcf3e002c9be
DuncanRitchie <duncanritchie@btinternet.com>
2019-08-19 17:31:51 +0100

    Button

--- Snake.java
+++ Snake.java
@@ -1,10 +1,23 @@
 package Snake;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Snake {
     private JPanel rootPanel;
     private JLabel snakeTitle;
+    private JButton startGameButton;
+    private JLabel textLabel;
+
+    public Snake() {
+        startGameButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                textLabel.setText("I've not yet written the game, sorry!");
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Snake");
